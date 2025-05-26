4184aacd677e00e7c1512b23fc48304414d960df
Kirliffilan <kiryxa09.06@gmail.com>
2025-05-26 16:28:30 +0300

    Структуризация проекта по папкам

--- KKKickers/GameObjects/Walls/BrokenWall.cs
+++ KKKickers/GameObjects/Walls/BrokenWall.cs
@@ -1,7 +1,7 @@
 ﻿using System.Drawing;
 using System.Numerics;
 
-namespace KKKickers
+namespace KKKickers.GameObjects.Walls
 {
     public class BrokenWall : Wall
     {
--- KKKickers/GameObjects/Walls/WinWall.cs
+++ KKKickers/GameObjects/Walls/WinWall.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using KKKickers.GameObjects.Walls;
 namespace KKKickers
 {
     public class WinWall : Wall
